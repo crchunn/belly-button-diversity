448a717e18eadf3ba5d0ea37df9011f48f4cbf09
Carrie Chunn <crchunn@gmail.com>
2020-08-19 13:44:51 -0500

    adding folders and code

---
+++ Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,18 @@
+// Use d3.json() to fetch data from JSON file
+// Incoming data is internally referred to as incomingData
+d3.json("data/samples.json").then((incomingData) => {
+    function filterGerms(germ) {
+      return germ.otu_id;
+    }
+
+    // use filter to pass function
+
+    var filteredGerms = incomingData.filtter (filterGerms);
+
+    // check check check
+
+    console.log(filteredGerms);
+
+
+
+});
