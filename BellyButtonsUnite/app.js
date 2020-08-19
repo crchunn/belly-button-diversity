19611e809a1daf4e06b57fcfb12946b3fb41a9ae
Carrie Chunn <crchunn@gmail.com>
2020-08-19 16:23:41 -0500

    plotting along

--- BellyButtonsUnite/app.js
+++ BellyButtonsUnite/app.js
@@ -1,19 +1,51 @@
 // Use d3.json() to fetch data from JSON file
-
-// Incoming data is internally referred to as incomingData
-d3.json("data/samples.json").then((incomingData) => {
-    function filterGerms(germ) {
-      return germ.otu_id;
-    }
-
-    // use filter to pass function
-
-    // var filteredGerms = incomingData.filter(filterGerms);
-
-    // check check check
-
+function unpack(rows, index) {
+  return rows.map(function (row) {
+    return row[index];
+  });
+}
+function parseBugs() {
+  d3.json("data/samples.json").then((incomingData) => {
+    var metadata = unpack()
+    buildTable(id, ethnicity, gender, age, location, bbtype, wfreq);
     console.log(incomingData);
-    
+
+    var filteredGerms = incomingData.filter(filterGerms);
+    // function filterGerms(germ) {
+    // return germ.id;
+    var bellyId = filteredGerms.map(germs => germs.id);
+
+    var samples = filteredGerms.map(germs => )
 
 
-});
+    var trace = {
+      x: bellyId,
+      y: samples,
+      type: "bar"
+    };
+    var data = [trace];
+
+    var layout = {
+      xaxis: { title: "OTU ID" },
+      yaxis: {title: "Number of Germs"}
+    };
+
+    Plotly.newPlot("bar-plot", data, layout);
+  });
+}
+// Incoming data is internally referred to as incomingData
+
+
+// use filter to pass function
+
+// gather information on otu_id and filter
+
+
+
+// check check check
+
+
+
+
+
+
