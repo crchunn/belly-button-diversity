92f4f2cfff22cd41c870cfba4e10bb0af45ee480
Carrie Chunn <crchunn@gmail.com>
2020-08-23 17:15:25 -0500

    adding colors

--- BellyButtonsUnite/app.js
+++ BellyButtonsUnite/app.js
@@ -5,70 +5,89 @@ function unpack(rows, index) {
   });
 }
 function charts(names) {
-d3.json("data/samples.json").then((incomingData) => {
-  // var metadata = unpack(incomingData.metadata, names)
-  // buildTable(id, ethnicity, gender, age, location, bbtype, wfreq);
-  var metadata = incomingData.metadata
-  console.log(metadata);
+  d3.json("data/samples.json").then((incomingData) => {
+    // var metadata = unpack(incomingData.metadata, names)
+    // buildTable(id, ethnicity, gender, age, location, bbtype, wfreq);
+    var metadata = incomingData.metadata
+    console.log(metadata);
 
-  var filteredGerms = metadata.filter((filterGerms) =>
-    // 
-    filterGerms.id == names);
+    var filteredGerms = metadata.filter((filterGerms) =>
+      // 
+      filterGerms.id == names);
 
-  console.log(filteredGerms);
+    console.log(filteredGerms);
 
-  var samples = incomingData.samples.filter((filterGerms) =>
-    filterGerms.id == names);
+    var samples = incomingData.samples.filter((filterGerms) =>
+      filterGerms.id == names);
     samples = samples[0]
-  console.log(samples);
+    console.log(samples);
 
-  var drop = d3.select("#sample-metadata");
-  drop.html("");
-  Object.entries(filteredGerms[0]).forEach(([key, value]) => {drop.append("h6").text(`${key}, ${value}`)})
+    var drop = d3.select("#sample-metadata");
+    drop.html("");
+    Object.entries(filteredGerms[0]).forEach(([key, value]) => { drop.append("h6").text(`${key}, ${value}`) })
 
-
-  var trace = {
-    
-    x: samples.sample_values,
-    y: samples.otu_ids,
-    text: samples.otu_labels,
-    name: "germy",
-    type: "bar", 
-    orientation: "h"
-  };
-  var data = [trace];
-
-  var layout = {
-    xaxis: { title: "OTU ID" },
-    yaxis: { title: "Number of Germs" }
-  };
-
-  Plotly.newPlot("bar", data, layout);
-
-  var trace2 = {
-    x: samples.otu_ids,
-    y: samples.sample_values,
-    text: samples.otu_labels,
-    mode: "markers",
-    type: "bubble",
-    // mode: 'markers',
-    // marker=dict(
-        // color=['rgb(44, 160, 101)'],
-        // size=[60],)
-  };
-  var data2 = [trace2];
-
-  var layout2 = {
-    xaxis: { title: "bubbleking" },
-    yaxis: { title: "grossness" }
-  };
-
-  Plotly.newPlot("bubble", data2, layout2);
+    var data = [{
+      type: 'bar',
+      x: samples.sample_values.slice(0, 10).reverse(),
+      y: samples.otu_ids,
+      text: samples.otu_labels,
+      orientation: 'h',
+      // color: 
+    }];
+    var layout = {
+      xaxis:{
+          autorange:'reversed'
+      },
+      // yaxis:{
+        // side:'left'
+      // }
+  }
 
 
+    Plotly.newPlot('bar', data, layout)
+
+    /* var trace = {
+
+      x: samples.sample_values.slice(0, 10).reverse(),
+      y: samples.otu_ids,
+      text: samples.otu_labels,
+      name: "germy",
+      type: "bar",
+      orientation: "h"
+    };
+    var data = [trace];
+
+    var layout = {
+      xaxis: { title: "OTU ID" },
+      yaxis: { title: "Number of Germs" }
+    };
+
+    Plotly.newPlot("bar", data, layout); */
+
+    var trace2 = {
+      x: samples.otu_ids,
+      y: samples.sample_values,
+      text: samples.otu_labels,
+      mode: 'markers',
+      marker: {
+        size: samples.sample_values,
+        color: samples.otu_ids,
+      }
+    };
+
+    var data = [trace2];
+
+    var layout2 = {
+      title: 'Marker Size',
+      showlegend: false,
+    };
+
+    Plotly.newPlot('myDiv', data, layout2);
 
 
-});
+
+
+  });
 }
 // charts(940)
 function parseBugs() {
@@ -82,31 +101,10 @@ function parseBugs() {
       dropDown.append("option").text(bellyBug).property("value", bellyBug);
     })
   });
-charts(940)
+  charts(940)
 };
 function optionChanged(newName) {
   charts(newName)
 
 };
 parseBugs()
-
-
-
-
-
-  // var samples = filteredGerms.map(germs => )
-
-
-
-// });
-// }
-// Incoming data is internally referred to as incomingData
-
-
-// use filter to pass function
-
-// gather information on otu_id and filter
-
-
-
-// check check check */
